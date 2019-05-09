0c4c555eb4ba7abc6be70ac188e2357313a53686
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-09 22:09:46 +0300

    purifier

---
+++ purifier.py
@@ -0,0 +1,31 @@
+import ast
+
+class Insecure(Exception):
+    pass
+
+def insecure(func):
+    def wrapper(instance, node):
+        try:
+            func(instance, node)
+        except Insecure:
+            raise Insecure(f"{node.__class__.__name__} found in line {node.lineno}:{node.col_offset}")
+
+    return wrapper
+    
+class Purifier(ast.NodeVisitor):
+    """Tries to dedect *basic* security falls. Of course it can
+    be hacked by using dot access model, bytecode etc. The purpose
+    is avoiding as much as we can"""
+    
+    @insecure
+    def visit_Import(*args):
+        raise Insecure
+    
+    @insecure
+    def visit_ImportFrom(*args):
+        raise Insecure
+    
+    @insecure
+    def visit_Attribute(self, node):
+        if isinstance(node.value, ast.NameConstant):
+            raise Insecure
