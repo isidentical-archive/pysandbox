830a4df070d3f0c612380824f3cc5a0b358bcc89
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-10 04:28:49 +0300

    Container caching

--- evality/__init__.py
+++ evality/__init__.py
@@ -3,6 +3,7 @@ import json
 import marshal
 import os
 import socket
+import sys
 import time
 from base64 import b64encode
 from contextlib import contextmanager
@@ -20,15 +21,18 @@ class Evality:
         self._api_client = api_client
         self._image = self.obtain_image()
 
+        self._instances = {}
         self._ports = {}
 
-        self._delay = 1
+        self._delay = 1.5
         self._purifier = Purifier()
 
-    def run_cmd(self, code):
+    def run_cmd(self, code, idx=0):
+        """Runs python code on the containers,
+        code is self-explaining, step-by-step"""
+
         tree = ast.parse(code)
         try:
-            # step by step for easy understanding
             self._purifier.visit(tree)
 
             code = compile(tree, "<evality>", "exec")
@@ -38,7 +42,7 @@ class Evality:
             payload = {"code": code.decode()}
             payload = json.dumps(payload)
 
-            with self.run_instance() as container:
+            with self.run_instance(idx) as container:
                 request = Request(
                     f"http://localhost:{self._ports[container]}",
                     data=payload.encode(),
@@ -54,17 +58,32 @@ class Evality:
         return response
 
     @contextmanager
-    def run_instance(self):
-        port = self._get_free_port()
-        container = self._docker_client.containers.run(
-            "evality", ports={"18888/tcp": port}, detach=True
-        )
-        self._ports[container] = port
+    def run_instance(self, idx):
+        new = self._instances.get(idx)
+        if new:
+            container = new
+        else:
+            port = self._get_free_port()
+            container = self._docker_client.containers.run(
+                "evality", ports={"18888/tcp": port}, detach=True
+            )
+            self._ports[container] = port
+            self._instances[idx] = container
         try:
-            time.sleep(self._delay)
+            if new:
+                container.unpause()
+            else:
+                time.sleep(self._delay)
             yield container
         finally:
-            container.kill()
+            container.pause()
+
+    def quit(self):
+        for container in self._instances.copy():
+            self.quit_single(container)
+
+    def quit_single(self, idx):
+        return self._instances.pop(idx).kill()
 
     def obtain_image(self):
         try:
--- evality/__main__.py
+++ evality/__main__.py
@@ -1,13 +1,23 @@
-import sys
+import time
+
 import docker
+
 from evality import Evality
 
-def main(evality, cmd):
-    res = evality.run_cmd(cmd.strip("'"))
-    print(res)
-    
+
+def main(evality):
+    cmd = "print('hello world')"
+    while cmd != "!q":
+        t0 = time.time()
+        res = evality.run_cmd(cmd)
+        t1 = time.time()
+        print(res, f"{t1 - t0} seconds")
+        cmd = input("cmd> ")
+    evality.quit()
+
+
 if __name__ == "__main__":
     docker_client = docker.from_env()
     api_client = docker.APIClient(base_url="unix://var/run/docker.sock")
     evality = Evality(docker_client, api_client)
-    main(evality, *sys.argv[1:])
+    main(evality)
